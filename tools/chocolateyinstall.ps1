aaa47bfc7dcdad6a067d1b28ebca008e897a85e3
Mateleo <mateleo78@gmail.com>
2026-01-03 15:34:44 +0100

    init

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$version    = '0.7.7.1'
+
+$url64      = "https://github.com/imputnet/helium-windows/releases/download/$version/Helium_$($version)_x64-installer.exe"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  
+  url           = '' 
+  url64bit      = $url64
+
+  softwareName  = 'helium*'
+
+  # 32-bit checksum (Empty)
+  checksum      = ''
+  checksumType  = 'sha256'
+
+  checksum64    = '3AFF957788E9189CC04EBD3CE3A2E5473A3185935117E0FA130DFFB369A15F0E'
+  checksumType64= 'sha256'
+
+  silentArgs    = '--do-not-launch-chrome' 
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,46 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = $env:ChocolateyPackageName
+$softwareName = 'Helium'
+
+$packageArgs = @{
+  packageName   = $packageName
+  softwareName  = $softwareName
+  fileType      = 'EXE'
+  silentArgs    = '--uninstall --force-uninstall' 
+  validExitCodes= @(0, 19)
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -ge 1) {
+    $key | ForEach-Object {
+        $uninstallString = $_.UninstallString
+        if ($uninstallString -match '"') {
+            $packageArgs['file'] = ($uninstallString -split '"')[1]
+        } else {
+            $packageArgs['file'] = $uninstallString
+        }
+
+        if (Test-Path $packageArgs['file']) {
+            try {
+                Uninstall-ChocolateyPackage @packageArgs
+            } catch {
+                Write-Warning "Standard uninstaller failed. Proceeding to manual cleanup..."
+            }
+        }
+    }
+}
+
+$userPath   = "$env:LOCALAPPDATA\imput\Helium"
+$systemPath = "$env:ProgramFiles\Helium"
+
+if (Test-Path $userPath) {
+    Write-Host "Removing leftover User files..."
+    Remove-Item -Path $userPath -Recurse -Force -ErrorAction SilentlyContinue
+}
+if (Test-Path $systemPath) {
+    Write-Host "Removing leftover System files..."
+    Remove-Item -Path $systemPath -Recurse -Force -ErrorAction SilentlyContinue
+}
+
+Write-Host "Uninstall complete."
