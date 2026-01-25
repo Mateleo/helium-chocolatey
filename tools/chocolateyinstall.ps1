65ace4a5ff4e4b3dda52ca36d88fa822e4f153df
Mateleo <Mateleo@users.noreply.github.com>
2026-01-25 00:59:08 +0000

    [create-pull-request] automated change

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$version    = '0.7.10.1'
+$version    = '0.8.3.1'
 
 $url64      = "https://github.com/imputnet/helium-windows/releases/download/$version/Helium_$($version)_x64-installer.exe"
 
@@ -19,7 +19,7 @@ $packageArgs = @{
   checksum      = ''
   checksumType  = 'sha256'
 
-  checksum64    = '5d88436723639986d6178e3b3b03f7f1d338d6975a9600f731212a92e7d727d4'
+  checksum64    = '4fc5f90ad56418a175e0ceb1af0350db4ca1e208a1de5260d9653a5a4aae5d86'
   checksumType64= 'sha256'
 
   silentArgs    = '--do-not-launch-chrome' 
