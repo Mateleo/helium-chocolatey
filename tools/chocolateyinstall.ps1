c2f471ae9a3fa6dce3a2a8d692c3ecf6574ba0c0
Mateleo <Mateleo@users.noreply.github.com>
2026-05-21 19:48:13 +0000

    [create-pull-request] automated change

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$version    = '0.12.1.1'
+$version    = '0.12.4.1'
 
 $url64      = "https://github.com/imputnet/helium-windows/releases/download/$version/Helium_$($version)_x64-installer.exe"
 
@@ -19,7 +19,7 @@ $packageArgs = @{
   checksum      = ''
   checksumType  = 'sha256'
 
-  checksum64    = '915a2244dc2d38ac32aa1b6ecbfd242514efb047934646d2901cdd15721f2ac7'
+  checksum64    = '4045e9c3958781bcb0f6b7200e85565dc4aa557928b25bcbc0994f0588f359cf'
   checksumType64= 'sha256'
 
   # NSIS installer flags:
